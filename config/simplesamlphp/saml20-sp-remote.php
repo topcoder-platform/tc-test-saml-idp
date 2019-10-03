9f20516f9fbaa52b4daca6bb976883a421eda9f7
Matthew Twomey <mtwomey@beakstar.com>
2019-10-02 21:30:25 -0500

    Add second provider

--- config/simplesamlphp/saml20-sp-remote.php
+++ config/simplesamlphp/saml20-sp-remote.php
@@ -5,7 +5,13 @@
  * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-sp-remote
  */
 
-$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
-    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
-    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
+$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID_01')] = array(
+    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE_01'),
+    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE_01'),
 );
+
+$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID_02')] = array(
+    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE_02'),
+    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE_02'),
+);
+~
